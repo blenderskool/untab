356c1762882d6a82e74163d1a99e9b1cc5f5527a
blenderskool <akash.hamirwasia@gmail.com>
2020-09-12 13:44:23 +0530

    Add search layout
    Structure and CSS for the search layout is added. Results are
    hardcoded as of now

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,80 @@
 <script>
+	let results = [
+		{
+			title: 'Tab 1',
+		},
+		{
+			title: 'Tab 2',
+		},
+		{
+			title: 'Tab 3',
+		}
+	]
 </script>
 
-<main>
-	UnTab
-</main>
+<div class="search-wrapper">
+
+	<div class="search">
+		<input type="search" placeholder="Search Tab..." >
+		<ul class="results">
+			{#each results as result}
+				<li>
+					{result.title}
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
+
+<style>
+
+	:global(body) {
+		font-size: 16px;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+		font-family: 'Lato', sans-serif;
+	}
+
+	.search-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(0, 0, 0, 0.5);
+		display: flex;
+		justify-content: center;
+		align-items: self-end;
+	}
+
+	.search {
+		background-color: #fff;
+		position: relative;
+		top: 90px;
+		width: 750px;
+		border-radius: 10px;
+		overflow: hidden;
+	}
+
+	.search input {
+		width: 100%;
+		border: none;
+		outline: none;
+		height: 50px;
+		padding: 0 16px;
+		font-weight: 700;
+		font-size: 1.2rem;
+	}
+
+	.results {
+		padding: 0;
+		margin: 0;
+		list-style: none;
+	}
+
+	.results li {
+		padding: 12px 16px;
+	}
+</style>
