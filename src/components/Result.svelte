dd8de8b2a65ca97d97e44bda831be29153ee8abf
blenderskool <akash.hamirwasia@gmail.com>
2020-09-12 19:35:22 +0530

    Add Result component

---
+++ src/components/Result.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let result;
+  let faviconError = false;
+</script>
+
+<li>
+  {#if result.favicon && !faviconError}
+    <img class="favicon" src={result.favicon} alt="" on:error={() => faviconError = true}>
+  {:else}
+    <span class="favicon-placeholder" />
+  {/if}
+
+  <div class="info" title={result.title}>
+    <h4 class="title">{result.title}</h4>
+    <p class="url">{result.url}</p>
+  </div>
+</li>
+
+
+<style>
+  li {
+    padding: 12px 16px 12px 20px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+  li:hover {
+    background-color: rgba(99, 179, 237, 0.45);
+  }
+
+  .favicon, .favicon-placeholder {
+    width: 16px;
+    margin-right: 16px;
+  }
+  .favicon-placeholder {
+    width: 12px;
+    height: 12px;
+    background-color: #ED8936;
+    border-radius: 100%;
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+    overflow-x: hidden;
+  }
+
+  .title, .url {
+    margin: 0;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow-x: hidden;
+  }
+
+  .url {
+    font-size: 12px;
+    color: #4A5568;
+    margin-top: 4px;
+  }
+</style>
--- src/components/Results.svelte
+++ src/components/Results.svelte
@@ -1,17 +1,13 @@
 <script>
+  import Result from './Result.svelte';
+
   import { results } from '../store';
 </script>
 
 {#if $results.length}
   <ul class="results">
     {#each $results as { item }}
-      <li>
-        <img class="favicon" src={item.favicon} alt="">
-        <div class="info">
-          <h4 class="title">{item.title}</h4>
-          <p class="url">{item.url}</p>
-        </div>
-      </li>
+      <Result result={item} />
     {/each}
   </ul>
 {/if}
@@ -24,30 +20,5 @@
     overflow-y: auto;
     max-height: 60vh;
     border-top: 1px solid #CECECE;
-  }
-
-  .results li {
-    padding: 12px 16px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-  }
-  .results li:hover {
-    background-color: rgba(99, 179, 237, 0.45);
-  }
-
-  .favicon {
-    width: 16px;
-    margin-right: 0.7rem;
-  }
-
-  .info {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
-  .title, .url {
-    margin: 0;
   }
 </style>
