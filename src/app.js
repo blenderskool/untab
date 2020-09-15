30f7c96a76393250b673394c00309354623df0c4
asharonbaltazar <asharonbaltazar@outlook.com>
2020-09-15 07:54:48 -0500

    Condition now knows classList.contains()

--- src/app.js
+++ src/app.js
@@ -25,5 +25,5 @@ window.addEventListener('message', ({ data: req }) => {
 });
 
 window.addEventListener('click', (e) => {
-  if(e.target.className.includes("search-wrapper")) close()
+  if(e.target.classList.contains("search-wrapper")) close()
 })
