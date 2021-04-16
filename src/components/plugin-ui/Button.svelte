402c5203a50a44e5df7fa91c68cdf5f199e8d9ec
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-04-16 18:33:56 +0530

    Add plugin UI components and alias to it

---
+++ src/components/plugin-ui/Button.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let title;
+</script>
+
+<div class="plugin-btn" title={title} on:click>
+  <slot />
+</div>
+
+<style>
+  .plugin-btn {
+    background-color: var(--bg-pill);
+    color: var(--gray-800);
+    padding: 0.2rem 0.4rem;
+    min-width: 25px;
+    min-height: 25px;
+    display: inline-flex;
+    border-radius: 4px;
+    border: 1px solid var(--gray-500);
+    margin-left: 8px;
+    font-weight: 600;
+    font-size: 0.875rem;
+    letter-spacing: 0.025rem;
+    transform: transition 0.2s ease;
+  }
+  .plugin-btn:first-of-type {
+    margin-left: 0;
+  }
+  .plugin-btn:hover {
+    transform: scale(1.05);
+  }
+
+  :global(.plugin-btn svg) {
+    width: 17px !important;
+  }
+</style>
