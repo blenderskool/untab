a1d739e28cb16a9604b310d1346649c234690bf9
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2022-01-13 16:07:35 +0530

    Fix iframe append error when body element is absent on the page
    Fixes #67

--- src/content/index.js
+++ src/content/index.js
@@ -4,19 +4,21 @@ const frame = document.createElement('iframe');
 frame.classList.add('untab');
 frame.src = browser.runtime.getURL('index.html');
 
-document.body.append(frame);
+if (document.body) {
+  document.body.append(frame);
 
-browser.runtime.onMessage.addListener((req) => {
-  switch(req.type) {
-    case constants.OPEN:
-      frame.classList.add('visible');
-      break;
-  }
-});
+  browser.runtime.onMessage.addListener((req) => {
+    switch(req.type) {
+      case constants.OPEN:
+        frame.classList.add('visible');
+        break;
+    }
+  });
 
-window.addEventListener('message', (e) => {
-  if (frame.contentWindow !== e.source) return;
+  window.addEventListener('message', (e) => {
+    if (frame.contentWindow !== e.source) return;
 
-  document.activeElement.blur();
-  frame.classList.remove('visible');
-});
+    document.activeElement.blur();
+    frame.classList.remove('visible');
+  });
+}
